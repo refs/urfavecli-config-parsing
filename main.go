2d74ef4558cfbcf2061901f3bf8f2beb59cb833c
A.Unger <zyxancf@gmail.com>
2021-04-30 14:16:45 +0200

    just use urfavecli

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/go-yaml/yaml"
+	"github.com/urfave/cli/v2/altsrc"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -34,67 +34,44 @@ type Config struct {
 
 func main() {
 	cfg := Config{}
-	flags := []cli.Flag{
-		&cli.StringFlag{
-			Name:        "proxy-port",
-			EnvVars:     []string{"PROXY_PORT"},
-			Value:       "3. default-value-flag-declaration",
-			Destination: &cfg.Proxy.Port,
-		},
-		&cli.StringFlag{
-			Name:        "proxy-addr",
-			EnvVars:     []string{"PROXY_ADDRESS"},
-			Value:       "3. default-value-flag-declaration",
-			Destination: &cfg.Proxy.Address,
-		},
-		&cli.StringSliceFlag{
-			Name: "patch",
-		},
+	rootFlags := []cli.Flag{
 		&cli.StringFlag{
 			Name: "config-file",
 		},
 	}
+	flags := []cli.Flag{
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "proxy-port",
+			EnvVars:     []string{"PROXY_PORT"},
+			Value:       "3. default-value-flag-declaration",
+			Destination: &cfg.Proxy.Port,
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:        "proxy-addr",
+			EnvVars:     []string{"PROXY_ADDRESS"},
+			Value:       "3. default-value-flag-declaration",
+			Destination: &cfg.Proxy.Address,
+		}),
+	}
 
 	app := &cli.App{
 		Action: func(c *cli.Context) error {
-			//return prettify(cfg)
-			return nil
+			return prettify(cfg)
 		},
-		Before: func(c *cli.Context) error {
-			// by the time we reach Before, flags have already been parsed
-			_ = prettify(cfg)
-			if c.IsSet("config-file") {
-				_ = parseConfig(c.Value("config-file").(string), &cfg)
-				if c.IsSet("patch") {
-					v := c.Value("patch").(cli.StringSlice)
-					for _, v := range v.Value() {
-						if len(strings.Split(v, "=")) != 2 {
-							break
-						}
-						left := strings.Split(v, "=")[0]
-						right := strings.Split(v, "=")[1]
-						if err := c.Set(left, right); err != nil {
-							return err
-						}
-					}
-				}
-				_ = prettify(cfg)
-			}
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:   "server",
+				Flags:  flags,
+				Before: altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("config-file")),
+				Action: func(context *cli.Context) error {
+					return prettify(cfg)
+				},
+			},
 		},
-		Flags: flags,
+		Flags: rootFlags,
 	}
 
 	app.Run(os.Args)
-}
-
-func parseConfig(file string, cfg *Config) error {
-	contents, err := os.ReadFile(file)
-	if err != nil {
-		return err
-	}
-
-	return yaml.Unmarshal(contents, &cfg)
 }
 
 func prettify(cfg Config) error {
